ef22c48cd37f064900509326b8bed1854f5d6c75
kazewong <kazewong.physics@gmail.com>
2025-06-11 09:33:11 -0400

    Make WaveformModel skeleton

--- src/ripplegw/waveform_model/WaveformModel.py
+++ src/ripplegw/waveform_model/WaveformModel.py
@@ -1,12 +1,16 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from jaxtyping import Float, Int, Array, PyTree
 
 class WaveformModel(ABC):
+
+    model_parameters: PyTree
+    
     def __init__(self) -> None:
         pass
 
-    def __call__(self, sample_points: Float[Array, " n_sample"], source_parameters: Float[Array, " n_params"], model_parameters: PyTree):
-        pass
-
-    def full_model(self, sample_axis:
-        False
+    def __call__(self, sample_points: Float[Array, " n_sample"], source_parameters: Float[Array, " n_params"]):
+       return self.full_model(sample_points, source_parameters, self.model_parameters) 
+        
+    @abstractmethod
+    def full_model(self, sample_points: Float[Array, " n_sample"],      source_parameters: Float[Array, " n_params"], model_parameters: PyTree):
+        raise NotImplementedError
