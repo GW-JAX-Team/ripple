7446ca3f34a1925089deaf3ab4222498de378721
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2025-11-19 17:18:43 +0800

    test: Add unit tests for xlal_imr_phenom_xp_check_masses_and_spins

---
+++ src/ripplegw/waveforms/__init__.py
@@ -0,0 +1,33 @@
+"""Initialization code for the ripplegw.waveforms package."""
+
+from __future__ import annotations
+
+from ripplegw.waveforms import (
+    IMRPhenom_tidal_utils,
+    IMRPhenomD,
+    IMRPhenomD_NRTidalv2,
+    IMRPhenomD_QNMdata,
+    IMRPhenomD_utils,
+    IMRPhenomPv2,
+    IMRPhenomPv2_utils,
+    IMRPhenomX_utils,
+    IMRPhenomXAS,
+    IMRPhenomXPHM,
+    SineGaussian,
+    TaylorF2,
+)
+
+__all__ = [
+    "IMRPhenomD",
+    "IMRPhenomD_NRTidalv2",
+    "IMRPhenomD_QNMdata",
+    "IMRPhenomD_utils",
+    "IMRPhenomPv2",
+    "IMRPhenomPv2_utils",
+    "IMRPhenomXAS",
+    "IMRPhenomXPHM",
+    "IMRPhenomX_utils",
+    "IMRPhenom_tidal_utils",
+    "SineGaussian",
+    "TaylorF2",
+]
---
+++ tests/imr_phenom_xphm/test_lal_sim_imr_phenom_x_utilities.py
@@ -0,0 +1,34 @@
+"""Unit tests for lal_sim_imr_phenom_x_utilities.py"""
+
+from __future__ import annotations
+
+from ripplegw.waveforms.IMRPhenomXPHM.lal_sim_imr_phenom_x_utilities import (
+    xlal_imr_phenom_xp_check_masses_and_spins,
+)
+
+
+def test_xlal_imr_phenom_xp_check_masses_and_spins():
+    """Test the xlal_imr_phenom_xp_check_masses_and_spins function.
+
+    Remarks: IMRPhenomXPCheckMassesAndSpins does not take arguments,
+        and therefore we cannot test it directly. Instead, we test
+        our wrapper function against the expected behavior.
+    """
+    # Test case where m1 > m2
+    m1, m2, chi1x, chi1y, chi1z, chi2x, chi2y, chi2z = (
+        30.0,
+        20.0,
+        0.1,
+        0.2,
+        0.3,
+        -0.1,
+        -0.2,
+        -0.3,
+    )
+    result = xlal_imr_phenom_xp_check_masses_and_spins(m1, m2, chi1x, chi1y, chi1z, chi2x, chi2y, chi2z)
+    assert result == (30.0, 20.0, 0.1, 0.2, 0.3, -0.1, -0.2, -0.3)
+
+    # Test case where m1 < m2
+    m1, m2 = 20.0, 30.0
+    result = xlal_imr_phenom_xp_check_masses_and_spins(m1, m2, chi1x, chi1y, chi1z, chi2x, chi2y, chi2z)
+    assert result == (30.0, 20.0, -0.1, -0.2, -0.3, 0.1, 0.2, 0.3)
