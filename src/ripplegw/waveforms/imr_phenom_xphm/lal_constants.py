75b1d6000431eab42307505be3989d1acdbf3eae
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2025-11-26 15:48:08 +0800

    feat: Add LAL_MTSUN_SI

--- src/ripplegw/waveforms/imr_phenom_xphm/lal_constants.py
+++ src/ripplegw/waveforms/imr_phenom_xphm/lal_constants.py
@@ -4,4 +4,8 @@ LAL constants.
 
 from __future__ import annotations
 
+# Nominal solar mass, kg
 LAL_MSUN_SI = 1.988409870698050731911960804878414216e30
+
+# Geometrized nominal solar mass, s
+LAL_MTSUN_SI = 4.925490947641266978197229498498379006e-6
