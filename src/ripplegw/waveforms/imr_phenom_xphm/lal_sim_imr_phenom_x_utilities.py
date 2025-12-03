35f91eae60b9408c98fc5ce030f64602be2a7f69
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2025-12-03 08:21:12 +0100

    fix: Fix pre-commit errors

--- src/ripplegw/waveforms/imr_phenom_xphm/lal_sim_imr_phenom_x_utilities.py
+++ src/ripplegw/waveforms/imr_phenom_xphm/lal_sim_imr_phenom_x_utilities.py
@@ -7,6 +7,7 @@ from jax import lax
 
 from ripplegw.constants import PI
 
+
 def xlal_imr_phenom_xp_check_masses_and_spins(  # pylint: disable=too-many-arguments,too-many-positional-arguments
     m1_si: float,
     m2_si: float,
@@ -130,146 +131,259 @@ def xlal_sim_imr_phenom_x_chi_pn_hat(eta: float, chi1l: float, chi2l: float) -> 
 
 
 # /**
-#  * Total spin normalised to [-1,1]
+#  * Total spin normalized to [-1,1]
 #  */
-def xlal_sim_imr_phenom_x_stot_r(eta: float, chi1L: float, chi2L: float) -> float:
-	# // Convention m1 >= m2 and chi1z is the spin projected along Lz on m1
-	delta = jnp.sqrt(1.0 - 4.0*eta)
-	m1    = 0.5*(1 + delta)
-	m2    = 0.5*(1 - delta)
-	m1s   = m1*m1
-	m2s   = m2*m2
+def xlal_sim_imr_phenom_x_stot_r(eta: float, chi1_l: float, chi2_l: float) -> float:
+    """Compute the total spin normalized to [-1, 1].
 
-	return ((m1s * chi1L + m2s * chi2L) / (m1s + m2s))
+    Args:
+        eta: Symmetric mass ratio.
+        chi1_l: Dimensionless spin component of the first body along the orbital angular momentum.
+        chi2_l: Dimensionless spin component of the second body along the orbital angular momentum.
+
+    Returns:
+        Total spin normalized to [-1, 1].
+    """
+    # // Convention m1 >= m2 and chi1z is the spin projected along Lz on m1
+    delta = jnp.sqrt(1.0 - 4.0 * eta)
+    m1 = 0.5 * (1 + delta)
+    m2 = 0.5 * (1 - delta)
+    m1s = m1 * m1
+    m2s = m2 * m2
+
+    return (m1s * chi1_l + m2s * chi2_l) / (m1s + m2s)
 
 
 # /**
 #  * Spin difference
 #  */
-def xlal_sim_imr_phenom_x_dchi(chi1L: float, chi2L: float) -> float:
-    return chi1L - chi2L
+def xlal_sim_imr_phenom_x_dchi(chi1_l: float, chi2_l: float) -> float:
+    """Compute the spin difference dchi = chi1_l - chi2_l.
+
+    Args:
+        chi1_l: Dimensionless spin component of the first body along the orbital angular momentum.
+        chi2_l: Dimensionless spin component of the second body along the orbital angular momentum.
+
+    Returns:
+        Spin difference dchi.
+    """
+    return chi1_l - chi2_l
 
 
 # /**
 #  * Final Mass = 1 - Energy Radiated,  X Jimenez-Forteza et al, PRD, 95, 064024, (2017), arXiv:1611.00332
 #  */
-def xlal_sim_imr_phenom_x_final_mass_2017(eta, chi1L, chi2L):
+def xlal_sim_imr_phenom_x_final_mass_2017(eta: float, chi1_l: float, chi2_l: float) -> float:
+    """Compute the final mass of the merged object.
 
-    delta  = jnp.sqrt(1.0 - 4.0*eta)
-    eta2   =  eta*eta
-    eta3   = eta2*eta
-    eta4   = eta3*eta
+    Args:
+        eta: Symmetric mass ratio.
+        chi1_l: Dimensionless spin component of the first body along the orbital angular momentum.
+        chi2_l: Dimensionless spin component of the second body along the orbital angular momentum.
 
-    S      = xlal_sim_imr_phenom_x_stot_r(eta,chi1L,chi2L)
-    S2     =  S*S
-    S3     = S2*S
+    Returns:
+        Final mass of the merged object.
+    """
 
-    dchi   = chi1L - chi2L
-    dchi2  = dchi*dchi
+    delta = jnp.sqrt(1.0 - 4.0 * eta)
+    eta2 = eta * eta
+    eta3 = eta2 * eta
+    eta4 = eta3 * eta
 
-    noSpin = 0.057190958417936644*eta + 0.5609904135313374*eta2 - 0.84667563764404*eta3 + 3.145145224278187*eta4
+    s = xlal_sim_imr_phenom_x_stot_r(eta, chi1_l, chi2_l)
+    s2 = s * s
+    s3 = s2 * s
+
+    dchi = chi1_l - chi2_l
+    dchi2 = dchi * dchi
+
+    no_spin = (
+        0.057190958417936644 * eta + 0.5609904135313374 * eta2 - 0.84667563764404 * eta3 + 3.145145224278187 * eta4
+    )
 
     #   /* Because of the way this is written, we need to subtract the noSpin term */
-    eqSpin = ((0.057190958417936644*eta + 0.5609904135313374*eta2 - 0.84667563764404*eta3 + 3.145145224278187*eta4)* \
-    (    1 \
-        + (-0.13084389181783257 - 1.1387311580238488*eta + 5.49074464410971*eta2)*S \
-        + (-0.17762802148331427 + 2.176667900182948*eta2)*S2 \
-        + (-0.6320191645391563 + 4.952698546796005*eta - 10.023747993978121*eta2)*S3)) \
-        / (1 + (-0.9919475346968611 + 0.367620218664352*eta + 4.274567337924067*eta2)*S)
+    eq_spin = (
+        (0.057190958417936644 * eta + 0.5609904135313374 * eta2 - 0.84667563764404 * eta3 + 3.145145224278187 * eta4)
+        * (
+            1
+            + (-0.13084389181783257 - 1.1387311580238488 * eta + 5.49074464410971 * eta2) * s
+            + (-0.17762802148331427 + 2.176667900182948 * eta2) * s2
+            + (-0.6320191645391563 + 4.952698546796005 * eta - 10.023747993978121 * eta2) * s3
+        )
+    ) / (1 + (-0.9919475346968611 + 0.367620218664352 * eta + 4.274567337924067 * eta2) * s)
 
-    eqSpin = eqSpin - noSpin
+    eq_spin = eq_spin - no_spin
 
-    uneqSpin =  - 0.09803730445895877*dchi*delta*(1 - 3.2283713377939134*eta)*eta2 \
-                + 0.01118530335431078*dchi2*eta3 \
-                - 0.01978238971523653*dchi*delta*(1 - 4.91667749015812*eta)*eta*S
+    uneq_spin = (
+        -0.09803730445895877 * dchi * delta * (1 - 3.2283713377939134 * eta) * eta2
+        + 0.01118530335431078 * dchi2 * eta3
+        - 0.01978238971523653 * dchi * delta * (1 - 4.91667749015812 * eta) * eta * s
+    )
 
-    #   /* Mfinal = 1 - Erad, assuming that M = m1 + m2 = 1 */
-    return (1.0 - (noSpin + eqSpin + uneqSpin))
+    #   /* M_final = 1 - E_rad, assuming that M = m1 + m2 = 1 */
+    return 1.0 - (no_spin + eq_spin + uneq_spin)
 
 
 # /**
 #  * Final Dimensionless Spin,  X Jimenez-Forteza et al, PRD, 95, 064024, (2017), arXiv:1611.00332
 #  */
-def xlal_sim_imr_phenom_x_final_spin_2017(eta, chi1L, chi2L):
-    delta  = jnp.sqrt(1.0 - 4.0*eta)
-    m1     = 0.5 * (1.0 + delta)
-    m2     = 0.5 * (1.0 - delta)
-    m1Sq   = m1*m1
-    m2Sq   = m2*m2
+def xlal_sim_imr_phenom_x_final_spin_2017(eta, chi1_l, chi2_l) -> float:  # pylint: disable=too-many-locals
+    """Compute the final dimensionless spin of the merged object.
 
-    eta2   = eta*eta
-    eta3   = eta2*eta
+    Args:
+        eta: Symmetric mass ratio.
+        chi1_l: Dimensionless spin component of the first body along the orbital angular momentum.
+        chi2_l: Dimensionless spin component of the second body along the orbital angular momentum.
+
+    Returns:
+        Final dimensionless spin of the merged object.
+    """
+    delta = jnp.sqrt(1.0 - 4.0 * eta)
+    m1 = 0.5 * (1.0 + delta)
+    m2 = 0.5 * (1.0 - delta)
+    m1_sq = m1 * m1
+    m2_sq = m2 * m2
+
+    eta2 = eta * eta
+    eta3 = eta2 * eta
 
     # //S  = (m1Sq * chi1L + m2Sq * chi2L) / (m1Sq + m2Sq)
-    S  = xlal_sim_imr_phenom_x_stot_r(eta,chi1L,chi2L)
-    S2 =  S*S
-    S3 = S2*S
+    s = xlal_sim_imr_phenom_x_stot_r(eta, chi1_l, chi2_l)
+    s2 = s * s
+    s3 = s2 * s
 
-    dchi  = chi1L - chi2L
-    dchi2 = dchi*dchi
+    dchi = chi1_l - chi2_l
+    dchi2 = dchi * dchi
 
-
-    noSpin = (3.4641016151377544*eta + 20.0830030082033*eta2 - 12.333573402277912*eta3)/(1 + 7.2388440419467335*eta)
-
-    eqSpin = (m1Sq + m2Sq)*S \
-    + ((-0.8561951310209386*eta - 0.09939065676370885*eta2 + 1.668810429851045*eta3)*S \
-    + (0.5881660363307388*eta - 2.149269067519131*eta2 + 3.4768263932898678*eta3)*S2 \
-    + (0.142443244743048*eta - 0.9598353840147513*eta2 + 1.9595643107593743*eta3)*S3) \
-    / (1 + (-0.9142232693081653 + 2.3191363426522633*eta - 9.710576749140989*eta3)*S)
-
-    uneqSpin = 0.3223660562764661*dchi*delta*(1 + 9.332575956437443*eta)*eta2 \
-    - 0.059808322561702126*dchi2*eta3 \
-    + 2.3170397514509933*dchi*delta*(1 - 3.2624649875884852*eta)*eta3*S \
-
-    return (noSpin + eqSpin + uneqSpin)
-
-
-def xlal_sim_imr_phenom_x_fMECO(eta, chi1L, chi2L):
-
-    eta2  = (eta*eta)
-    eta3  = (eta2*eta)
-    eta4  = (eta3*eta)
-
-    delta = jnp.sqrt(1.0 - 4.0*eta)
-
-    S     = xlal_sim_imr_phenom_x_chi_pn_hat(eta,chi1L,chi2L)
-    S2    = (S*S)
-    S3    = (S2*S)
-    #//S4    = (S3*S)
-
-    dchi  = chi1L - chi2L
-    dchi2 = (dchi*dchi)
-
-    noSpin = (0.018744340279608845 + 0.0077903147004616865*eta + 0.003940354686136861*eta2 - 0.00006693930988501673*eta3)/(1. - 0.10423384680638834*eta)
-
-    eqSpin = (S*(0.00027180386951683135 - 0.00002585252361022052*S + eta4*(-0.0006807631931297156 + 0.022386313074011715*S - 0.0230825153005985*S2) + eta2*(0.00036556167661117023 - 0.000010021140796150737*S - 0.00038216081981505285*S2) + eta*(0.00024422562796266645 - 0.00001049013062611254*S - 0.00035182990586857726*S2) + eta3*(-0.0005418851224505745 + 0.000030679548774047616*S + 4.038390455349854e-6*S2) - 0.00007547517256664526*S2))/(0.026666543809890402 + (-0.014590539285641243 - 0.012429476486138982*eta + 1.4861197211952053*eta4 + 0.025066696514373803*eta2 + 0.005146809717492324*eta3)*S + (-0.0058684526275074025 - 0.02876774751921441*eta - 2.551566872093786*eta4 - 0.019641378027236502*eta2 - 0.001956646166089053*eta3)*S2 + (0.003507640638496499 + 0.014176504653145768*eta + 1.*eta4 + 0.012622225233586283*eta2 - 0.00767768214056772*eta3)*S3)
-
-    uneqSpin = dchi2*(0.00034375176678815234 + 0.000016343732281057392*eta)*eta2 + dchi*delta*eta*(0.08064665214195679*eta2 + eta*(-0.028476219509487793 - 0.005746537021035632*S) - 0.0011713735642446144*S)
-
-    return (noSpin + eqSpin + uneqSpin)
-
-
-def xlal_sim_imr_phenom_x_fISCO(chif):
-
-    Z1 = 1.0 + (1.0 - chif*chif)**(1/3) * ((1 + chif)**(1/3) + (1 - chif)**(1/3))
-    Z1 = lax.select( #Finite precission may give Z1>3, but this can not happen.
-        Z1 > 3,
-        3.0,
-        Z1
+    no_spin = (3.4641016151377544 * eta + 20.0830030082033 * eta2 - 12.333573402277912 * eta3) / (
+        1 + 7.2388440419467335 * eta
     )
-    Z2 = jnp.sqrt(3.0*chif*chif + Z1*Z1)
 
-    rISCO    = 3.0 + Z2 - xlal_sim_imr_phenom_x_sign(chif)*jnp.sqrt( (3 - Z1) * (3 + Z1 + 2*Z2) )
-    rISCOsq  = jnp.sqrt(rISCO)
-    rISCO3o2 = rISCOsq * rISCOsq * rISCOsq
+    eq_spin = (m1_sq + m2_sq) * s + (
+        (-0.8561951310209386 * eta - 0.09939065676370885 * eta2 + 1.668810429851045 * eta3) * s
+        + (0.5881660363307388 * eta - 2.149269067519131 * eta2 + 3.4768263932898678 * eta3) * s2
+        + (0.142443244743048 * eta - 0.9598353840147513 * eta2 + 1.9595643107593743 * eta3) * s3
+    ) / (1 + (-0.9142232693081653 + 2.3191363426522633 * eta - 9.710576749140989 * eta3) * s)
 
-    OmegaISCO = 1.0 / ( rISCO3o2 + chif)
+    uneq_spin = (
+        0.3223660562764661 * dchi * delta * (1 + 9.332575956437443 * eta) * eta2
+        - 0.059808322561702126 * dchi2 * eta3
+        + 2.3170397514509933 * dchi * delta * (1 - 3.2624649875884852 * eta) * eta3 * s
+    )
+    return no_spin + eq_spin + uneq_spin
 
-    return OmegaISCO / PI 
+
+def xlal_sim_imr_phenom_x_f_meco(eta: float, chi1_l: float, chi2_l: float) -> float:
+    """Compute the MECO frequency.
+
+    Args:
+        eta: Symmetric mass ratio.
+        chi1_l: Dimensionless spin component of the first body along the orbital angular momentum.
+        chi2_l: Dimensionless spin component of the second body along the orbital angular momentum.
+
+    Returns:
+        MECO frequency.
+    """
+
+    eta2 = eta * eta
+    eta3 = eta2 * eta
+    eta4 = eta3 * eta
+
+    delta = jnp.sqrt(1.0 - 4.0 * eta)
+
+    s = xlal_sim_imr_phenom_x_chi_pn_hat(eta, chi1_l, chi2_l)
+    s2 = s * s
+    s3 = s2 * s
+    # //S4    = (S3*S)
+
+    dchi = chi1_l - chi2_l
+    dchi2 = dchi * dchi
+
+    no_spin = (
+        0.018744340279608845 + 0.0077903147004616865 * eta + 0.003940354686136861 * eta2 - 0.00006693930988501673 * eta3
+    ) / (1.0 - 0.10423384680638834 * eta)
+
+    eq_spin = (
+        s
+        * (
+            0.00027180386951683135
+            - 0.00002585252361022052 * s
+            + eta4 * (-0.0006807631931297156 + 0.022386313074011715 * s - 0.0230825153005985 * s2)
+            + eta2 * (0.00036556167661117023 - 0.000010021140796150737 * s - 0.00038216081981505285 * s2)
+            + eta * (0.00024422562796266645 - 0.00001049013062611254 * s - 0.00035182990586857726 * s2)
+            + eta3 * (-0.0005418851224505745 + 0.000030679548774047616 * s + 4.038390455349854e-6 * s2)
+            - 0.00007547517256664526 * s2
+        )
+    ) / (
+        0.026666543809890402
+        + (
+            -0.014590539285641243
+            - 0.012429476486138982 * eta
+            + 1.4861197211952053 * eta4
+            + 0.025066696514373803 * eta2
+            + 0.005146809717492324 * eta3
+        )
+        * s
+        + (
+            -0.0058684526275074025
+            - 0.02876774751921441 * eta
+            - 2.551566872093786 * eta4
+            - 0.019641378027236502 * eta2
+            - 0.001956646166089053 * eta3
+        )
+        * s2
+        + (
+            0.003507640638496499
+            + 0.014176504653145768 * eta
+            + 1.0 * eta4
+            + 0.012622225233586283 * eta2
+            - 0.00767768214056772 * eta3
+        )
+        * s3
+    )
+
+    uneq_spin = dchi2 * (0.00034375176678815234 + 0.000016343732281057392 * eta) * eta2 + dchi * delta * eta * (
+        0.08064665214195679 * eta2
+        + eta * (-0.028476219509487793 - 0.005746537021035632 * s)
+        - 0.0011713735642446144 * s
+    )
+
+    return no_spin + eq_spin + uneq_spin
 
 
-def xlal_sim_imr_phenom_x_sign(x):
-    return lax.select( #1.0 if x > 0. else (-1.0 if x < 0.0 else 0.0)
+def xlal_sim_imr_phenom_x_f_isco(chi_f: float) -> float:
+    """Compute the ISCO frequency.
+
+    Args:
+        chi_f: Dimensionless spin of the final black hole.
+
+    Returns:
+        ISCO frequency.
+    """
+
+    z1 = 1.0 + (1.0 - chi_f * chi_f) ** (1 / 3) * ((1 + chi_f) ** (1 / 3) + (1 - chi_f) ** (1 / 3))
+    z1 = lax.select(z1 > 3, 3.0, z1)  # Finite precision may give Z1>3, but this can not happen.
+    z2 = jnp.sqrt(3.0 * chi_f * chi_f + z1 * z1)
+
+    r_isco = 3.0 + z2 - xlal_sim_imr_phenom_x_sign(chi_f) * jnp.sqrt((3 - z1) * (3 + z1 + 2 * z2))
+    r_isco_sq = jnp.sqrt(r_isco)
+    r_isco_3o2 = r_isco_sq * r_isco_sq * r_isco_sq
+
+    omega_isco = 1.0 / (r_isco_3o2 + chi_f)
+
+    return omega_isco / PI
+
+
+def xlal_sim_imr_phenom_x_sign(x: float) -> float:
+    """Return the sign of x: 1.0 if x > 0, -1.0 if x < 0, 0.0 if x == 0.
+
+    Args:
+        x: Input value.
+
+    Returns:
+        Sign of x.
+    """
+    return lax.select(  # 1.0 if x > 0. else (-1.0 if x < 0.0 else 0.0)
         x > 0.0,
         1.0,
         lax.select(
