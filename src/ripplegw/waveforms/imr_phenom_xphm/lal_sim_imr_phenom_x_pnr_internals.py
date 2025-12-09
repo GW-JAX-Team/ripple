fefb1324d906782a8c594567177537eb4ceb04d6
robkamcha <robkamcha@gmail.com>
2025-12-09 14:25:41 +0100

    feat: Add IMRPhenomX_PNR_GetAndSetPNRVariables

--- src/ripplegw/waveforms/imr_phenom_xphm/lal_sim_imr_phenom_x_pnr_internals.py
+++ src/ripplegw/waveforms/imr_phenom_xphm/lal_sim_imr_phenom_x_pnr_internals.py
@@ -1,149 +1,170 @@
+"""
+Docstring for ripplegw.waveforms.imr_phenom_xphm.lal_sim_imr_phenom_x_pnr_internals
+"""
+
+from __future__ import annotations
+
+import dataclasses
+
 import jax
 import jax.numpy as jnp
 
-from ripplegw.constants import PI, C, G
+from ripplegw.constants import PI
 from ripplegw.waveforms.imr_phenom_xphm.lal_sim_imr_phenom_x_internals_dataclass import (
     IMRPhenomXWaveformDataClass,
 )
 from ripplegw.waveforms.imr_phenom_xphm.lal_sim_imr_phenom_x_precession_dataclass import (
     IMRPhenomXPrecessionDataClass,
 )
+from ripplegw.waveforms.imr_phenom_xphm.lal_sim_imr_phenom_x_utilities import (
+    xlal_sim_imr_phenom_x_chi_eff,
+    xlal_sim_imr_phenom_x_final_spin_2017,
+)
+
 
 def imr_phenom_x_pnr_get_and_set_pnr_variables(
-    p_wf: IMRPhenomXWaveformDataClass,    
-    p_prec: IMRPhenomXPrecessionDataClass
-  ):
+    p_wf: IMRPhenomXWaveformDataClass, p_prec: IMRPhenomXPrecessionDataClass
+):
+    """
+    Docstring for imr_phenom_x_pnr_get_and_set_pnr_variables
 
-    #/* get needed quantities */
+    :param p_wf: Description
+    :type p_wf: IMRPhenomXWaveformDataClass
+    :param p_prec: Description
+    :type p_prec: IMRPhenomXPrecessionDataClass
+    """
+    # https://lscsoft.docs.ligo.org/lalsuite/lalsimulation/_l_a_l_sim_i_m_r_phenom_x___p_n_r__internals_8c.html#aabb3d2fee26595c5a632df2df8832836
+
+    # /* get needed quantities */
     m1 = p_wf.m1 * p_wf.m_tot
     m2 = p_wf.m2 * p_wf.m_tot
     q = p_wf.q
 
-#     if(p_prec.imr_phenom_x_prec_version == 330)
-#     {
-#       chi1x = pPrec->chi1x_evolved;
-#       chi1y = pPrec->chi1y_evolved;
-#       chi2x = pPrec->chi2x_evolved;
-#       chi2y = pPrec->chi2y_evolved;
-#       chieff = XLALSimIMRPhenomXchiEff(pWF->eta,pPrec->chi1z_evolved,pPrec->chi2z_evolved);
-#     }
-#     else
-#     {
-#       chi1x = pPrec->chi1x;
-#       chi1y = pPrec->chi1y;
-#       chi2x = pPrec->chi2x;
-#       chi2y = pPrec->chi2y;
-#       chieff = XLALSimIMRPhenomXchiEff(pWF->eta,pPrec->chi1z,pPrec->chi2z);
-#     }
+    chi1x, chi1y, chi2x, chi2y, chieff = jax.lax.select(
+        p_prec.imr_phenom_x_prec_version == 330,
+        jnp.array(
+            [
+                p_prec.chi1x_evolved,
+                p_prec.chi1y_evolved,
+                p_prec.chi2x_evolved,
+                p_prec.chi2y_evolved,
+                xlal_sim_imr_phenom_x_chi_eff(p_wf.eta, p_prec.chi1z_evolved, p_prec.chi2z_evolved),
+            ]
+        ),
+        jnp.array(
+            [
+                p_prec.chi1x,
+                p_prec.chi1y,
+                p_prec.chi2x,
+                p_prec.chi2y,
+                xlal_sim_imr_phenom_x_chi_eff(p_wf.eta, p_prec.chi1z, p_prec.chi2z),
+            ]
+        ),
+    )
 
-#     chipar = pWF->Mtot * chieff / m1;
-#     chiperp = 0.0;
-#     costheta = 0.0;
-#     chiperp_antisymmetric = 0.0;
-#     theta_antisymmetric = 0.0;
+    chipar = p_wf.m_tot * chieff / m1
+    chiperp = 0.0
+    costheta = 0.0
+    chiperp_antisymmetric = 0.0
+    theta_antisymmetric = 0.0
 
-#     /* compute effective in-plane spin contribution from Eq. 17 of arXiv:2107.08876 */
-#     /* for XO4a this contribution is only used for mass ratios below 1.5 */
-#     /* in versions of the model where we use the evolved spin values, we use this contribution at all mass ratios */
-#     if(pPrec->IMRPhenomXPrecVersion == 330){
-#           chis = sqrt((m1 * m1 * chi1x + m2 * m2 * chi2x) * (m1 * m1 * chi1x + m2 * m2 * chi2x) + (m1 * m1 * chi1y + m2 * m2 * chi2y) * (m1 * m1 * chi1y + m2 * m2 * chi2y)) / (m1 * m1);
-# 	  chiperp = chis;
-#     }
-#     else{
-#       if (q <= 1.5)
-# 	{
-# 	  chis = sqrt((m1 * m1 * chi1x + m2 * m2 * chi2x) * (m1 * m1 * chi1x + m2 * m2 * chi2x) + (m1 * m1 * chi1y + m2 * m2 * chi2y) * (m1 * m1 * chi1y + m2 * m2 * chi2y)) / (m1 * m1);
-# 	  chiperp = sin((q - 1.0) * LAL_PI) * sin((q - 1.0) * LAL_PI) * pPrec->chi_p + cos((q - 1.0) * LAL_PI) * cos((q - 1.0) * LAL_PI) * chis;
-# 	}
-#       else
-# 	{
-# 	  chiperp = pPrec->chi_p;
-# 	}
-#     }
+    # /* compute effective in-plane spin contribution from Eq. 17 of arXiv:2107.08876 */
+    # /* for XO4a this contribution is only used for mass ratios below 1.5 */
+    # /* in versions of the model where we use the evolved spin values, we use this contribution at all mass ratios */
 
-#     if (q <= 1.5)
-#       {
-# 	antisymmetric_chis = sqrt((m1 * m1 * chi1x - m2 * m2 * chi2x) * (m1 * m1 * chi1x - m2 * m2 * chi2x) + (m1 * m1 * chi1y - m2 * m2 * chi2y) * (m1 * m1 * chi1y - m2 * m2 * chi2y)) / (m1 * m1);
-# 	chiperp_antisymmetric = sin((q - 1.0) * LAL_PI) * sin((q - 1.0) * LAL_PI) * pPrec->chi_p + cos((q - 1.0) * LAL_PI) * cos((q - 1.0) * LAL_PI) * antisymmetric_chis;
-#       }
-#     else
-#       {
-# 	chiperp_antisymmetric = pPrec->chi_p;
-#       }
+    def prec_version_330_branch():
+        chis = jnp.sqrt(
+            (m1 * m1 * chi1x + m2 * m2 * chi2x) * (m1 * m1 * chi1x + m2 * m2 * chi2x)
+            + (m1 * m1 * chi1y + m2 * m2 * chi2y) * (m1 * m1 * chi1y + m2 * m2 * chi2y)
+        ) / (m1 * m1)
+        return chis
 
-#     /* get the total magnitude, Eq. 18 of arXiv:2107.08876 */
-#     chi_mag = sqrt(chipar * chipar + chiperp * chiperp);
-#     pPrec->chi_singleSpin = chi_mag;
+    def other_prec_version_branch():
+        chis = jnp.sqrt(
+            (m1 * m1 * chi1x + m2 * m2 * chi2x) * (m1 * m1 * chi1x + m2 * m2 * chi2x)
+            + (m1 * m1 * chi1y + m2 * m2 * chi2y) * (m1 * m1 * chi1y + m2 * m2 * chi2y)
+        ) / (m1 * m1)
 
-#     chi_mag_antisymmetric = sqrt(chipar * chipar + chiperp_antisymmetric * chiperp_antisymmetric);
-#     pPrec->chi_singleSpin_antisymmetric = chi_mag_antisymmetric;
+        return jax.lax.select(
+            q <= 1.5,
+            jnp.sin((q - 1.0) * PI) * jnp.sin((q - 1.0) * PI) * p_prec.chi_p
+            + jnp.cos((q - 1.0) * PI) * jnp.cos((q - 1.0) * PI) * chis,
+            p_prec.chi_p,
+        )
 
-#     /* get the opening angle of the single spin, Eq. 19 of arXiv:2107.08876 */
-#     if (chi_mag >= 1.0e-6)
-#     {
-#       costheta = chipar / chi_mag;
-#     }
-#     else
-#     {
-#       costheta = 0.;
-#     }
-#     if (chi_mag_antisymmetric >= 1.0e-6)
-#     {
-#       theta_antisymmetric = acos(chipar / chi_mag_antisymmetric);
-#     }
-#     else
-#     {
-#       theta_antisymmetric = 0.0;
-#     }
+    chiperp = jax.lax.cond(
+        p_prec.imr_phenom_x_prec_version == 330,
+        prec_version_330_branch(),
+        other_prec_version_branch(),
+    )
 
-#     pPrec->costheta_singleSpin = costheta;
-#     pPrec->theta_antisymmetric = theta_antisymmetric;
+    antisymmetric_chis = jnp.sqrt(
+        (m1 * m1 * chi1x - m2 * m2 * chi2x) * (m1 * m1 * chi1x - m2 * m2 * chi2x)
+        + (m1 * m1 * chi1y - m2 * m2 * chi2y) * (m1 * m1 * chi1y - m2 * m2 * chi2y)
+    ) / (m1 * m1)
+    chiperp_antisymmetric = jax.lax.select(
+        q <= 1.5,
+        jnp.sin((q - 1.0) * PI) * jnp.sin((q - 1.0) * PI) * p_prec.chi_p
+        + jnp.cos((q - 1.0) * PI) * jnp.cos((q - 1.0) * PI) * antisymmetric_chis,
+        p_prec.chi_p,
+    )
 
-#     /* compute an approximate final spin using single-spin mapping FIXME: add documentation */
-#     chi1L = chi_mag * costheta;
-#     chi2L = 0.0;
+    # /* get the total magnitude, Eq. 18 of arXiv:2107.08876 */
+    chi_mag = jnp.sqrt(chipar * chipar + chiperp * chiperp)
+    # pPrec->chi_singleSpin = chi_mag
 
-#     Xfparr = XLALSimIMRPhenomXFinalSpin2017(pWF->eta, chi1L, chi2L);
+    chi_mag_antisymmetric = jnp.sqrt(chipar * chipar + chiperp_antisymmetric * chiperp_antisymmetric)
+    # pPrec->chi_singleSpin_antisymmetric = chi_mag_antisymmetric
 
-#     /* rescale Xfperp to use the final total mass of 1 */
-#     qfactor = q / (1.0 + q);
-#     Xfperp = qfactor * qfactor * chi_mag * sqrt(1.0 - costheta * costheta);
-#     xf = sqrt(Xfparr * Xfparr + Xfperp * Xfperp);
-#     if (xf > 1.0e-6)
-#     {
-#       pPrec->costheta_final_singleSpin = Xfparr / xf;
-#     }
-#     else
-#     {
-#       pPrec->costheta_final_singleSpin = 0.;
-#     }
+    # /* get the opening angle of the single spin, Eq. 19 of arXiv:2107.08876 */
+    costheta = jax.lax.select(
+        chi_mag >= 1.0e-6,
+        chipar / chi_mag,
+        0.0,
+    )
+    theta_antisymmetric = jax.lax.select(
+        chi_mag_antisymmetric >= 1.0e-6,
+        jnp.arccos(chipar / chi_mag_antisymmetric),
+        0.0,
+    )
 
-#     /* Initialize frequency values */
-#     pPrec->PNR_HM_Mflow = 0.0;
-#     pPrec->PNR_HM_Mfhigh = 0.0;
+    # /* compute an approximate final spin using single-spin mapping */
+    chi1l = chi_mag * costheta
+    chi2l = 0.0
 
-#     /* set angle window boundaries */
-#     pPrec->PNR_q_window_lower = 8.5;
-#     pPrec->PNR_q_window_upper = 12.0;
-#     pPrec->PNR_chi_window_lower = 0.85;
-#     pPrec->PNR_chi_window_upper = 1.2;
+    x_fparr = xlal_sim_imr_phenom_x_final_spin_2017(p_wf.eta, chi1l, chi2l)
 
-#     /* set inspiral scaling flag for HM frequency map */
-#     pPrec->PNRInspiralScaling = 0;
-#     if ((q > pPrec->PNR_q_window_upper) || (pPrec->chi_singleSpin > pPrec->PNR_chi_window_upper))
-#     {
-#       pPrec->PNRInspiralScaling = 1;
-#     }
+    # /* rescale Xfperp to use the final total mass of 1 */
+    qfactor = q / (1.0 + q)
+    x_fperp = qfactor * qfactor * chi_mag * jnp.sqrt(1.0 - costheta * costheta)
+    xf = jnp.sqrt(x_fparr * x_fparr + x_fperp * x_fperp)
 
-#     #if DEBUG == 1
-#       printf("\nSetting PNR-related single-spin quantities:\n");
-#       printf("chi_singleSpin                     : %e\n", pPrec->chi_singleSpin);
-#       printf("chi_singleSpin_antisymmetric       : %e\n", pPrec->chi_singleSpin_antisymmetric);
-#       printf("costheta_singleSpin                : %e\n", pPrec->costheta_singleSpin);
-#       /* printf("theta_singleSpin_antisymmetric  : %e\n", pPrec->theta_singleSpin_antisymmetric); */
-#       printf("theta_antisymmetric  : %e\n", pPrec->theta_antisymmetric);
-#       printf("costheta_final_singleSpin          : %e\n\n", pPrec->costheta_final_singleSpin);
-#     #endif
+    costheta_final_single_spin = jax.lax.select(
+        xf > 1.0e-6,
+        x_fparr / xf,
+        0.0,
+    )
 
-#     return XLAL_SUCCESS;
-#   }
+    # /* set inspiral scaling flag for HM frequency map */
+    scaling_condition = (q > p_prec.pnr_q_window_upper) | (chi_mag > p_prec.pnr_chi_window_upper)
+    pnr_inspiral_scaling = jax.lax.select(
+        scaling_condition,
+        1,
+        0,
+    )
+
+    p_prec = dataclasses.replace(
+        p_prec,
+        chi_single_spin=chi_mag,
+        chi_single_spin_antisymmetric=chi_mag_antisymmetric,
+        cos_theta_single_spin=costheta,
+        theta_antisymmetric=theta_antisymmetric,
+        cos_theta_final_single_spin=costheta_final_single_spin,
+        pnr_hm_m_f_low=0.0,
+        pnr_hm_m_f_high=0.0,
+        pnr_q_window_lower=8.5,
+        pnr_q_window_upper=12.0,
+        pnr_chi_window_lower=0.85,
+        pnr_chi_window_upper=1.2,
+        pnr_inspiral_scaling=pnr_inspiral_scaling,
+    )
