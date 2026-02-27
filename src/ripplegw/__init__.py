8a72dbac3266a2a68dee0d47cc028c3d3b649243
Thomas Ng <thomas.ng@nikhef.nl>
2026-02-27 18:55:23 +0100

    Revert last commit

--- src/ripplegw/__init__.py
+++ src/ripplegw/__init__.py
@@ -1,11 +1,3 @@
-from ripplegw.conversions import (
-    Mc_eta_to_ms,
-    ms_to_Mc_eta,
-    lambdas_to_lambda_tildes,
-    lambdas_to_lambda_tildes_from_q,
-    lambda_tildes_to_lambdas,
-    lambda_tildes_to_lambdas_from_q,
-)
 from ripplegw.waveforms.IMRPhenomD import gen_IMRPhenomD_hphc
 from ripplegw.waveforms.IMRPhenomD_NRTidalv2 import gen_IMRPhenomD_NRTidalv2_hphc
 from ripplegw.waveforms.IMRPhenomXAS import gen_IMRPhenomXAS_hphc
@@ -15,14 +7,6 @@ from ripplegw.waveforms.TaylorF2 import gen_TaylorF2_hphc
 from ripplegw.waveforms.SineGaussian import gen_SineGaussian_hphc
 
 __all__ = [
-    # Conversion utilities
-    "Mc_eta_to_ms",
-    "ms_to_Mc_eta",
-    "lambdas_to_lambda_tildes",
-    "lambdas_to_lambda_tildes_from_q",
-    "lambda_tildes_to_lambdas",
-    "lambda_tildes_to_lambdas_from_q",
-    # Waveform generators
     "gen_IMRPhenomD_hphc",
     "gen_IMRPhenomD_NRTidalv2_hphc",
     "gen_IMRPhenomXAS_hphc",
