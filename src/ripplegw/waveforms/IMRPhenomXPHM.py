b1a46f5ecda335aee2ef0ed7812e3d9625bfa1f3
Thomas Ng <thomas.ng@nikhef.nl>
2026-03-09 12:26:15 +0100

    Remove typing.py and update import in IMRPhenomXPHM.py to use jaxtyping.Array

--- src/ripplegw/waveforms/IMRPhenomXPHM.py
+++ src/ripplegw/waveforms/IMRPhenomXPHM.py
@@ -3,7 +3,7 @@ from jax import jit
 import jax.numpy as jnp
 from .IMRPhenomD_QNMdata import QNMData_a, QNMData_fRD, QNMData_fdamp
 from ..constants import C, PI, MSUN, MTSUN_SI, MTSUN, MRSUN, MPC
-from ..typing import Array
+from jaxtyping import Array
 from .spherical_harmonics import (
     compute_sminus2_l2,
     compute_sminus2_l3,
