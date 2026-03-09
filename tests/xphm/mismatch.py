b19985f15620eace5e64df743605ee0622c42181
Narola Harsh <51940885+narolaharsh@users.noreply.github.com>
2026-03-09 01:37:15 +0100

    Catch errors for the try statement

--- tests/xphm/mismatch.py
+++ tests/xphm/mismatch.py
@@ -264,7 +264,7 @@ def compute_mismatch_loop(
 
 
 def main():
-    N_injections = int(100)
+    N_injections = int(1e4)
     seed = 200
     np.random.seed(seed)
     bilby.core.utils.random.seed(seed)
