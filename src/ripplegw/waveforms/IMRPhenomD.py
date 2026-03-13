Fix Seta calculation to use absolute value for stability in IMRPhenomD and related modules

src/ripplegw/waveforms/IMRPhenomD.py
@@ -222,7 +222,7 @@ def get_inspiral_Amp(fM_s: Array, theta: Array, coeffs: Array) -> Array:
     eta2 = eta * eta
     eta3 = eta * eta2
 
-    Seta = jnp.sqrt(1.0 - 4.0 * eta)
+    Seta = jnp.sqrt(jnp.abs(1.0 - 4.0 * eta))
     SetaPlus1 = 1.0 + Seta
 
     # Spin variables

src/ripplegw/waveforms/IMRPhenomD_utils.py
@@ -38,7 +38,7 @@ def EradRational0815_s(eta, s):
 
 
 def EradRational0815(eta, chi1, chi2):
-    Seta = jnp.sqrt(1.0 - 4.0 * eta)
+    Seta = jnp.sqrt(jnp.abs(1.0 - 4.0 * eta))
     m1 = 0.5 * (1.0 + Seta)
     m2 = 0.5 * (1.0 - Seta)
     m1s = m1 * m1
@@ -181,7 +181,7 @@ def get_coeffs(theta: Array) -> Array:
     # Definition of chiPN from lalsuite
     chi_s = (chi1 + chi2) / 2.0
     chi_a = (chi1 - chi2) / 2.0
-    seta = (1 - 4 * eta) ** (1 / 2)
+    seta = jnp.abs(1 - 4 * eta) ** (1 / 2)
     chiPN = chi_s * (1 - 76 * eta / 113) + seta * chi_a
 
     coeff = (

src/ripplegw/waveforms/IMRPhenomPv2.py
@@ -37,7 +37,7 @@ def PhenomPCoreTwistUp(
 
     # here it is used to be LAL_MTSUN_SI
     f = fHz * MTSUN * M  # Frequency in geometric units
-    q = (1.0 + jnp.sqrt(1.0 - 4.0 * eta) - 2.0 * eta) / (2.0 * eta)
+    q = (1.0 + jnp.sqrt(jnp.abs(1.0 - 4.0 * eta)) - 2.0 * eta) / (2.0 * eta)
     m1 = 1.0 / (1.0 + q)  # Mass of the smaller BH for unit total mass M=1.
     m2 = q / (1.0 + q)  # Mass of the larger BH for unit total mass M=1.
     Sperp = chip * (

src/ripplegw/waveforms/IMRPhenomPv2_utils.py
@@ -28,7 +28,7 @@ def ROTATEY(angle, x, y, z):
 
 
 def FinalSpin0815(eta, chi1, chi2):
-    Seta = jnp.sqrt(1.0 - 4.0 * eta)
+    Seta = jnp.sqrt(jnp.abs(1.0 - 4.0 * eta))
     m1 = 0.5 * (1.0 + Seta)
     m2 = 0.5 * (1.0 - Seta)
     m1s = m1 * m1