e9d9a7695e8657ec813f9a82526e57e2a8112eda
Thomas Ng <thomas.ng@nikhef.nl>
2026-04-01 16:29:11 +0200

    fix: simplify LALSuite availability check and remove debug print statement

--- tests/utils.py
+++ tests/utils.py
@@ -23,7 +23,7 @@ try:
 
     LAL_AVAILABLE = True
 except ImportError:
-    import traceback, sys; sys.stdout.write("IMPORT ERROR:\n"); traceback.print_exc(file=sys.stdout); LAL_AVAILABLE = False
+    LAL_AVAILABLE = False
 
 
 def check_lal_available():
@@ -678,4 +678,3 @@ def generate_random_params(
         )
 
     return theta
-print('LAL_AVAILABLE is', LAL_AVAILABLE)
