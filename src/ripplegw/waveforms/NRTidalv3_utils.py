6e8f15032234fd61f0e004312675854392d3e48e
Thomas Ng <thomas.ng@nikhef.nl>
2026-04-05 15:34:49 +0200

    fix: update comment for clarity on frequency conversion in _get_merger_frequency function

--- src/ripplegw/waveforms/NRTidalv3_utils.py
+++ src/ripplegw/waveforms/NRTidalv3_utils.py
@@ -96,7 +96,7 @@ def _get_merger_frequency(theta: Array):
 
     Momega_merger = nu * Qfit * TWO_PI
 
-    # convert from angular frequency to frequency (divide by 2*pi) and then convert from dimensionless frequency to Hz (divide by mtot * LAL_MTSUN)
+    # convert from angular frequency to frequency (divide by TWO_PI) and then convert from dimensionless frequency to Hz (divide by M * MTSUN)
     fHz_merger = Momega_merger / TWO_PI / (M * MTSUN)
 
     return fHz_merger
