6ff93079cf0e26865837ed2a8f19247164e50095
Thomas Ng <thomas.ng@nikhef.nl>
2026-05-06 23:28:02 +0200

    fix: Update mismatch threshold for IMRPhenomD_NRTidalv2

--- tests/cross_validation/test_lal_mismatch.py
+++ tests/cross_validation/test_lal_mismatch.py
@@ -86,7 +86,7 @@ MISMATCH_THRESHOLDS = {
     "IMRPhenomD": 1e-6,
     "IMRPhenomXAS": 1e-13,
     "IMRPhenomXHM": 1e-6,
-    "IMRPhenomD_NRTidalv2": 1e-9,
+    "IMRPhenomD_NRTidalv2": 1e-8,
     "IMRPhenomXAS_NRTidalv3": 1e-6,
     "TaylorF2": 1e-14,
     "IMRPhenomPv2": 1e-4,  # see note above
